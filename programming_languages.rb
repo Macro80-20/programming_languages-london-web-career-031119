22efc0524cab079d3023aae4dd8de1a514f3e042
fernando <fernando_lopezc@live.com>
2019-03-06 11:29:18 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,76 @@
+require "pry"
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
 def reformat_languages(languages)
-  # your code here
+  array={}
+  languages.each do |la,data|
+    languages[la].each do |x,y|
+      array[x] = y.merge({:style => [la]})
+  #binding.pry
+    end
+  end
+  array[:javascript][:style].unshift(:oo)
+  array
+  #binding.pry
 end
+reformat_languages(languages)
+#binding.pry
+
+{
+  :ruby=>{
+  :type=>"interpreted",
+  :style=>[:oo]
+},
+ :javascript=>{
+   :type=>"interpreted",
+   :style=>[:functional]
+ },
+ :python=>{
+   :type=>"interpreted",
+   :style=>[:oo]
+ },
+ :java=>{
+   :type=>"compiled",
+   :style=>[:oo]
+ },
+ :clojure=>{
+   :type=>"compiled",
+   :style=>[:functional]
+ },
+ :erlang=>{
+   :type=>"compiled",
+   :style=>[:functional]
+ },
+ :scala=>{
+   :type=>"compiled", :style=>[:functional]
+   }
+ }
